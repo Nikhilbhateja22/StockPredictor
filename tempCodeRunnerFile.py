f0c65fe8f69747a837dacde90f4ccfa87d891b97
gunmaykochar <Gunmaykochar11@gmail.com>
2025-04-27 00:08:23 +0530

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from flask import Flask, render_template, request, send_file
+import datetime as dt
+import yfinance as yf
+from sklearn.preprocessing import MinMaxScaler
+import os
